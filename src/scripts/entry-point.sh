686d1d7def65917b81d6cefeeab5e111ab3e9917
ahMAD ASSadi <ahmadasadihengami@gmail.com>
2025-09-18 00:33:38 +0330

    refactor: update load-scripts.sh and entry-point.sh to improve error handling and streamline database migration and static file collection

--- src/scripts/entry-point.sh
+++ src/scripts/entry-point.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-source env.sh
+set -e
 
-WORKERS=$(python -c "import os; print(os.cpu_count())")
-PORT=$(python -c "import os; print(os.getenv('PORT', '8000'))")
-python manage.py migrate
+# WORKERS=$(python -c "import os; print(os.cpu_count())")
+# PORT=$(python -c "import os; print(os.getenv('PORT', '8000'))")
+echo "Applying database migrations..."
+python manage.py migrate --noinput
 
 python manage.py collectstatic --noinput
 
-gunicorn --bind 0.0.0.0:"$PORT" --workers "$WORKERS" config.wsgi:application
+# gunicorn --bind 0.0.0.0:"$PORT" --workers "$WORKERS" config.wsgi:application
+
+exec "$@"
